d57282eefbab49d92f85f2739324321e5c9e4f64
Bruna de Mello Almeida <almeida.bruna@ime.eb.br>
2024-03-30 01:33:42 -0300

    adding modifications in order to generalize code

--- irace-logs/main.py
+++ irace-logs/main.py
@@ -1,52 +1,40 @@
 from WordCount import WordCountSparkCassandraIrace
 from helpers.parser import build_parser
+from helpers.irace_metadata import save_irace_metadata
 from datetime import datetime, timedelta
-import json
 import sys
-import argparse
-
 
 bash_parameters = sys.argv
 
 args = build_parser().parse_args()
 
-date_ref = (datetime.now() -  timedelta(hours=3)).date().strftime("%Y-%m-%d")
+date_ref = (datetime.now() - timedelta(hours=3)).date().strftime("%Y-%m-%d")
 
 print(datetime.now(), 'Starting process')
-
-flag_irace = True
-
-all_parameters = ["executorMemory", "driverCores", "sqlShufflePartitions", "defaultParallelism", 
-                    "memoryFraction", "shuffleCompress", "partitionOverwriteMode", "cassandraOutputConsistencyLevel", 
-                    "cassandraInputSplitSizeinMB", "cassandraOutputBatchSizeRows", "cassandraOutputBatchGroupingBufferSize",
-                    "cassandraOutputConcurrentWrites"]
 
 configuration_id = args.configId
 instance_id = args.instanceId
 seed = args.seed
 instance = args.instance
 
+file_path = f'/home/bruna/irace/files/{instance}'
+
 parameters = {}
 
-for parameter in all_parameters:
-    if parameter == 'executorMemory':
-        parameters[parameter] = str(getattr(args, parameter)) + 'G'
-    else:
-        parameters[parameter] = getattr(args, parameter)
+for key, value in vars(args).items():
+    if value != None:
+        if key == 'executorMemory':
+            parameters[key] = str(value) + 'G'
+        else:
+            parameters[key] = value
 
 wordcount_obj = WordCountSparkCassandraIrace(parameters)
-
-file_path = f'/home/bruna/irace/files/{instance}'
 
 try:
     begin = datetime.now()
     wordcount_obj.logs_word_count(path=file_path)
-    #total_rows = wordcount_obj.read_and_filter_data()
-    #print("Total de IPs filtrados:", total_rows)
     end = datetime.now()
     total = (end - begin).total_seconds() 
-
-    print(wordcount_obj.spark_config)
         
     execution = True
 
@@ -54,24 +42,31 @@ except Exception as error:
     end = datetime.now()
     execution = False
     print(error)
+    # adding an arbitrary value high enough to not impact the final result 
+    # probably this confguration will be discarded when Irace is running
     total = 100000000
 
-print(total)
 
-irace_metadata = {
-        "execution_id":str(bash_parameters[1]), 
-        "instance_id":instance_id, 
-        "configuration_id": configuration_id, 
-        "parameters":parameters, 
-        "begin":str(begin), 
-        "end":str(end), 
-        "total":str(total), 
-        "execution_status":execution
+# Building dict with irace metadata to save every execution of this script
+    
+if instance_id is not None:
+    # if instance_id is not None it means that Irace is running
+    # because this value is definied by Irace
+
+    irace_metadata = {
+        'instance_id': instance_id,
+        'configuration_id': configuration_id,
+        'parameters': parameters,
+        'begin': begin, 
+        'end': end,
+        'total': total,
+        'execution_status': execution
     }
 
-file = open(f'logs/irace-metadata-{date_ref}.txt', 'a')
-file.write(json.dumps(irace_metadata))
-file.write('\n')
-file.close()
+    save_irace_metadata(date_ref, irace_metadata)
+
+# DO NOT REMOVE THIS PRINT AND DO NOT PRINT ANYTHING ELSE AFTER THIS
+# THE VALUE WILL BE USE BY IRACE
+print(total)
 
 wordcount_obj.spark_stop()
