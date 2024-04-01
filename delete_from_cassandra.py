f47db75eb7b2b1a2c9835825ab39aad4c713afa3
Bruna de Mello Almeida <almeida.bruna@ime.eb.br>
2024-03-31 23:08:31 -0300

    repository organization

--- delete_from_cassandra.py
+++
@@ -1,6 +0,0 @@
-from cassandra.cluster import Cluster
-
-cluster = Cluster(['localhost'])
-session = cluster.connect()
-
-session.execute("TRUNCATE TABLE analytics_data.logs")
--- transform_metrics_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-df = pd.read_json('test_parameters.txt', orient='records', lines=True)
-
-df.to_csv('test_parameters.csv', index=False)
